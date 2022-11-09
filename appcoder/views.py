11213450e7aac8298d1154508ec0c9ab5d5fb5ec
Mauricio Lamboglia <mauricio.lamboglia@gmail.com>
2022-11-09 11:07:31 +0100

    Templates renovados

--- appcoder/views.py
+++ appcoder/views.py
@@ -5,16 +5,16 @@ from django.template import Template, Context, loader
 
 # Create your views here.
 def inicio(request):
-    return render(request, 'appcoder/index.html')
+    return render(request, 'appcoder/inicio.html')
 
 def cursos(request):
-    return HttpResponse('Estas en cursos')
+    return render(request, 'appcoder/cursos.html')
 
 def estudiantes(request):
-    return HttpResponse('Estas en estudiantes')
+    return render(request, 'appcoder/estudiantes.html')
 
 def profesores(request):
-    return HttpResponse('Estas en profesores')
+    return render(request, 'appcoder/profesores.html')
 
 def entregables(request):
-    return HttpResponse('Estas en entregables')
+    return render(request, 'appcoder/entregables.html')
