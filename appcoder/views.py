db07638949dc59584e71bcd1931aca28eefe34e9
Mauricio Lamboglia <mauricio.lamboglia@gmail.com>
2022-11-13 14:37:40 +0100

    prueba de git

--- appcoder/views.py
+++ appcoder/views.py
@@ -2,19 +2,73 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime
 from django.template import Template, Context, loader 
+from appcoder.models import *
+from appcoder.forms import *
 
-# Create your views here.
+
+
+
 def inicio(request):
     return render(request, 'appcoder/inicio.html')
 
 def cursos(request):
+    
     return render(request, 'appcoder/cursos.html')
+
+
+def creacion_curso(request):
+    
+    if request.method == 'POST':
+       
+        nombre_curso = request.POST['curso']
+        numero_camada = request.POST['camada']
+        curso = Curso(nombre=nombre_curso, camada=numero_camada)
+        curso.save()
+    
+    return render(request, 'appcoder/curso_formulario.html')
+
+def buscar_curso(request):
+    return render(request, 'appcoder/busqueda_cursos.html')
+
+
+def resultado_busqueda_cursos(request):
+     
+     nombre_curso = request.GET('nombre_curso')
+     cursos = Curso.objects.filter(nombre_icontains=nombre_curso)
+    return render(request,'appcoder/resultados_busqueda_cursos.html')
+
+
 
 def estudiantes(request):
     return render(request, 'appcoder/estudiantes.html')
 
+
 def profesores(request):
     return render(request, 'appcoder/profesores.html')
 
+
+def creacion_profesores(request):
+
+    if request.method == 'POST':
+        formulario = ProfesorFormulario(request.POST)
+       
+        if formulario.is_valid():
+           
+            data = formulario.cleaned_data
+
+        profesor = Profesor(nombre=data['nombre'], apellido=data['apellido'], email=data['email'], profesion=data['profesion'])
+
+        profesor.save()
+    
+
+    formulario = ProfesorFormulario()
+    
+    contexto = {'formulario': formulario}
+
+    return render(request, 'appcoder/profesores_formulario.html', contexto)
+
+
+
 def entregables(request):
     return render(request, 'appcoder/entregables.html')
+
